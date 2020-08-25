a93b58cbd5f7c49f9ace5ac230299db976364717
Andrew  Pacheco <ndrewpacheco@gmail.com>
2020-08-25 19:31:31 -0400

    fin easytest

---
+++ exercises/easytesting.rb
@@ -0,0 +1,56 @@
+require 'minitest/autorun'
+require "minitest/reporters"
+Minitest::Reporters.use!
+
+
+#1
+# assert value.odd?, "Value is not odd"
+
+#2
+
+# assert_equal( 'xyz', value.downcase)
+
+# #3
+
+# assert_nil value
+
+
+
+# 4
+
+
+# Write a minitest assertion that will fail if the Array list is not empty.
+
+# assert_empty list
+
+
+
+
+# 5
+# assert_includes list, 'xyz'
+
+
+
+
+# 6
+
+# assert_raises(NoExperienceError) { employee.hire }
+
+
+
+
+# 7
+# assert_instance_of Numeric, value
+
+
+# 8
+
+assert_kind_of Numeric, value
+
+# 9
+
+assert_same list, list.process
+
+# 10
+
+refute_includes list, 'xyz'
